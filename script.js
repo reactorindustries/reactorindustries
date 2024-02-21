e93cb1406d0121f28df54631940dddf7eea47f84
Some Random Programmer <115122130+gdi-main@users.noreply.github.com>
2024-02-21 15:59:58 -0500

    Create script.js
    asdf

---
+++ script.js
@@ -0,0 +1,4 @@
+function grab(id){
+    return document.getElementById(id);
+}
+var sloganList = ["ayo where'd the slogan go?", "Stupid Software Solutions™", "We Make Bugs 🐛", "help", "", "", "", "", "", ""];
