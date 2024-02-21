ca7f419f75e31773c844e305dec256f76b7317f8
Katsu <64220236+demonickatsu@users.noreply.github.com>
2024-02-21 17:21:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var sloganList = ["ayo where'd the slogan go?", "Stupid Software Solutions™", "we make bugs 🐛", "dafk.net/what", "I □ Unicode", "curb your enthusiasm", "made with TempleOS (not)", "we slightly know what we're doing", "Stack Overflow is our therapist", "we're not geniuses, but we're the closest you'll get for this price"];
 var randomSlogan = sloganList[Math.floor(Math.random()*9)]
 function randomizeSlogan(){
-  document.getElementById(slogan).innerHTML = randomSlogan;
+  document.getElementById("slogan").innerHTML = randomSlogan;
 }
 
 document.onload = randomizeSlogan()
