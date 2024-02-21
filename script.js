384878333a96bb358ae7e1cca787008c4de40d3f
Some Random Programmer <115122130+gdi-main@users.noreply.github.com>
2024-02-21 16:54:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-function grab(id){
-    return document.getElementById(id);
-}
+function grab(id) {return document.getElementById(id);}
 var sloganList = ["ayo where'd the slogan go?", "Stupid Software Solutions™", "we make bugs 🐛", "dafk.net/what", "I □ Unicode", "curb your enthusiasm", "made with TempleOS (not)", "we slightly know what we're doing", "Stack Overflow is our therapist", "we're not geniuses, but we're the closest you'll get for this price"];
 grab("slogan").innerHTML = sloganList[Math.floor(Math.random()*9)];
