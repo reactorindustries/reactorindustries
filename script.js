a51c8fa4d0a54af8a27893d95a92fee5db56af81
Some Random Programmer <115122130+gdi-main@users.noreply.github.com>
2024-02-23 18:57:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var sloganList = ["ayo where'd the slogan go?", "Stupid Software Solutions™", "we make bugs 🐛", "dafk.net/what", "I □ Unicode", "curb your enthusiasm", "made with TempleOS (not)", "we slightly know what we're doing", "Stack Overflow is our therapist", "we're not geniuses, but we're the closest you'll get for this price"];
 var randomSecret;
 var randomSlogan;
-var cheatedSlogan;
+var cheatedSlogan = true;
 function randomizeSlogan(){
   randomSlogan = sloganList[Math.floor(Math.random() * 9)];
   randomSecret = Math.floor(Math.random() * 99);
