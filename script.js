6143853f2ebbb35d74275bf16325155112df0f05
Some Random Programmer <115122130+gdi-main@users.noreply.github.com>
2024-02-23 19:15:32 -0500

    asdf

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var sloganList = ["ayo where'd the slogan go?", "Stupid Software Solutions™", "we make bugs 🐛", "dafk.net/what", "I □ Unicode", "curb your enthusiasm", "made with TempleOS (not)", "we slightly know what we're doing", "Stack Overflow is our therapist", "we're not geniuses, but we're the closest you'll get for this price"];
 var randomSecret;
 var randomSlogan;
-var cheatedSecret = true;
+var cheatedSecret = {cheater = true};
 function randomizeSlogan(){
   randomSlogan = sloganList[Math.floor(Math.random() * 9)];
   randomSecret = Math.floor(Math.random() * 99);
   document.getElementById("slogan").innerHTML = randomSlogan;
   if(randomSecret == 69){
-    cheatedSecret = false;
+    cheatedSecret.cheater = false;
     window.location.href = "secret.html";
   }
 }
-document.onload = randomizeSlogan()
+document.onload = randomizeSlogan();
